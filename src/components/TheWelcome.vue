595dc83ce9569ceb49e3001e22186e949efeb3a6
pohsengl <poh.seng.lim@intel.com>
2022-07-06 11:09:14 +0800

    Initial Commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+import ToolingIcon from "./icons/IconTooling.vue";
+import EcosystemIcon from "./icons/IconEcosystem.vue";
+import CommunityIcon from "./icons/IconCommunity.vue";
+import SupportIcon from "./icons/IconSupport.vue";
+import audio from '../assets/audio1.wav'
+
+function playAudio(){
+  let a=new Audio(audio)
+  a.play()
+}
+
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Community</template>
+
+    <button type="button" @click="playAudio">Click me</button>
+
+  </WelcomeItem>
+</template>
