93a6eace6a6a1ddd527c84fde320ca42efa3eb8e
pohsengl <poh.seng.lim@intel.com>
2022-07-14 12:31:20 +0800

    Integrate with backend api

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -8,26 +8,46 @@ import axios from 'axios'
 
 const textinput = ref('')
 const showOutput = ref(false)
-const audioURL=ref('')
+const audioURL = ref('')
+const backendChoice = ref('')
+
+function clearAll() {
+  textinput.value = ''
+  showOutput.value = false
+  audioURL.value = ''
+  backendChoice.value = ''
+}
 
 
 function submitText() {
   console.log(textinput.value)
-  postText(textinput.value)
+  console.log("Post")
+  postText(textinput.value, backendChoice.value)
 }
 
-async function postText(textinput: string) {
+async function postText(textinput: string, backendChoice: string) {
+
   try {
-    const POSTURL = 'https://jsonplaceholder.typicode.com/todos'
+    let POSTURL: string
+    if (backendChoice === 'lambda') {
+      POSTURL = 'https://cwi64lxsw0.execute-api.us-west-2.amazonaws.com/silero-lambda'
+    }
+    else if (backendChoice === 'ecs') {
+      POSTURL = 'https://lpssss.site/tts/'
+    }
+    else {
+      POSTURL = 'https://cwi64lxsw0.execute-api.us-west-2.amazonaws.com/silero-lambda'
+    }
     const data = {
-      id: 1,
       text: textinput
     }
     const response = await axios.post(POSTURL, data);
     console.log(response);
-    if (response.status === 201){
+    if (response.status === 201 || response.status === 200) {
       showOutput.value = true
-      audioURL.value="https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav"
+      console.log(response)
+      audioURL.value = 'https://d20d6d2exg5ld3.cloudfront.net/' + response.data.audioURL
+      //audioURL.value='https://d20d6d2exg5ld3.cloudfront.net/audio-f06e1ee6-099c-4278-a7c1-cc5e1c40b699.wav'
     }
   } catch (error) {
     console.error(error);
@@ -46,6 +66,14 @@ async function postText(textinput: string) {
 
     <textarea v-model="textinput" />
     <br>
+    <div>
+      <input type="radio" id="lambda" value="lambda" v-model="backendChoice" />
+      <label for="lambda">Lambda</label>
+    </div>
+    <div>
+      <input type="radio" id="ecs" value="ecs" v-model="backendChoice" />
+      <label for="ecs">ECS</label>
+    </div>
     <button type="button" @click="submitText">Submit</button>
 
   </WelcomeItem>
@@ -53,9 +81,11 @@ async function postText(textinput: string) {
     <template #icon>
       <ToolingIcon />
     </template>
-    <template #heading>Output Speech</template>
+    <template #heading>Output Speech ({{ backendChoice }})</template>
 
     <audio controls :src="audioURL"></audio>
-
+    <div>
+      <button type="button" @click="clearAll">Submit</button>
+    </div>
   </WelcomeItem>
 </template>
